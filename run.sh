2f617dddf1e3e1e161106b2ce273cbf44ac1e81a
oooooba <fuumin.23@gmail.com>
2022-11-22 23:47:12 +0900

    add sanitize support for generated code

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ cd ..
 
 gcc -Wall -Wextra -Werror -std=c11 -g \
     -fstrict-aliasing -Wstrict-aliasing \
+    -fsanitize=undefined -fno-sanitize-recover=all \
     -o $bin_file_name $c_file_name target/debug/libgogogogogo.a \
     -lpthread -ldl -lm \
 
