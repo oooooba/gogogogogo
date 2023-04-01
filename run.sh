88f178a192b0b5a70cfb52236d5afe56e969a6e8
oooooba <fuumin.23@gmail.com>
2023-04-01 23:09:30 +0900

    fix run.sh to handle files outside this repository

--- run.sh
+++ run.sh
@@ -5,8 +5,11 @@ set -e
 c_file_name=/tmp/out.c
 bin_file_name=/tmp/bin.exe
 
+dir_name=$(cd `dirname $1` && pwd)
+base_name=`basename $1`
+src=$dir_name/$base_name
 cd cgen
-go run main.go -i ../xtests/`basename $1` >$c_file_name
+go run main.go -i $src >$c_file_name
 cd ..
 
 gcc -Wall -Wextra -Werror -std=c11 -g \
