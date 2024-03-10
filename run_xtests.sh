66211fa6df852810f3862574067560a19303c6f3
oooooba <fuumin.23@gmail.com>
2024-03-10 22:00:26 +0900

    extend test harness to handle panic

--- run_xtests.sh
+++ run_xtests.sh
@@ -15,18 +15,37 @@ for path in xtests/*; do
     fi
 
     expect_result=/tmp/raw_expect_$base.txt
-    go run $path >$expect_result 2>&1
     actual_result=/tmp/raw_actual_$base.txt
-    bash ./run.sh $path >$actual_result 2>&1
 
-    compare_result=/tmp/compare_$base.txt
-    if diff -y $expect_result $actual_result >$compare_result; then
-        echo PASS
-    else
-        echo FAIL
-        cat $compare_result
-        exit_status=1
-    fi
+    case $base in
+        panic_*)
+            go run $path 2>&1 | head -n 1 >$expect_result || true
+            if ! bash ./run.sh $path >$actual_result 2>&1; then
+                if diff -y $expect_result $actual_result >$compare_result; then
+                    echo PASS
+                else
+                    echo FAIL
+                    cat $compare_result
+                    exit_status=1
+                fi
+            else
+                echo "FAIL (exit normaly)"
+                exit_status=1
+            fi
+            ;;
+        *)
+            go run $path >$expect_result 2>&1
+            bash ./run.sh $path >$actual_result 2>&1
+            compare_result=/tmp/compare_$base.txt
+            if diff -y $expect_result $actual_result >$compare_result; then
+                echo PASS
+            else
+                echo FAIL
+                cat $compare_result
+                exit_status=1
+            fi
+            ;;
+        esac
 done
 
 exit $exit_status
