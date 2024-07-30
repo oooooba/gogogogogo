ff031e150b7a4d3d36d4505f22f2303418ecfa46
oooooba <fuumin.23@gmail.com>
2024-07-30 22:22:58 +0900

    refactor lwt_spawn API

--- src/api/lwt.rs
+++ src/api/lwt.rs
@@ -5,24 +5,13 @@ use crate::FunctionObject;
 use crate::StackFrameCommon;
 use crate::UserFunction;
 
-#[repr(C)]
-struct StackFrameLwtSpawn {
-    common: StackFrameCommon,
-    func: FunctionObject,
-    result_size: usize,
-    args: WordChunk,
-}
-
-#[no_mangle]
-pub extern "C" fn gox5_lwt_spawn(ctx: &mut LightWeightThreadContext) -> FunctionObject {
+fn spawn<F>(ctx: &mut LightWeightThreadContext, param: F) -> FunctionObject
+where
+    F: FnOnce(&LightWeightThreadContext) -> (FunctionObject, usize, &WordChunk),
+{
+    let (entry_func, result_size, args) = param(ctx);
     let new_ctx = {
         let global_context = ctx.global_context().dupulicate();
-
-        let frame = ctx.stack_frame_mut::<StackFrameLwtSpawn>();
-        let entry_func = frame.func.clone();
-        let result_size = frame.result_size;
-        let args = frame.args.as_slice();
-
         let mut new_ctx = create_light_weight_thread_context(global_context, entry_func);
         let prev_stack_pointer = new_ctx.stack_pointer();
         let result_pointer = if result_size > 0 {
@@ -34,7 +23,7 @@ pub extern "C" fn gox5_lwt_spawn(ctx: &mut LightWeightThreadContext) -> Function
         new_ctx.push_frame(
             prev_stack_pointer,
             result_pointer,
-            args,
+            args.as_slice(),
             FunctionObject::from_user_function(UserFunction::new(crate::terminate)),
         );
         new_ctx
@@ -44,6 +33,25 @@ pub extern "C" fn gox5_lwt_spawn(ctx: &mut LightWeightThreadContext) -> Function
     });
     ctx.suspend();
     ctx.pop_frame()
+}
+
+#[repr(C)]
+struct StackFrameLwtSpawn {
+    common: StackFrameCommon,
+    func: FunctionObject,
+    result_size: usize,
+    args: WordChunk,
+}
+
+#[no_mangle]
+pub extern "C" fn gox5_lwt_spawn(ctx: &mut LightWeightThreadContext) -> FunctionObject {
+    spawn(ctx, |ctx| {
+        let frame = ctx.stack_frame::<StackFrameLwtSpawn>();
+        let entry_func = frame.func.clone();
+        let result_size = frame.result_size;
+        let args = &frame.args;
+        (entry_func, result_size, args)
+    })
 }
 
 #[no_mangle]
