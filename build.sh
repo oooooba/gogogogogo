41d2e5a3f5d950822b1ce1d24d3300e1a52995d1
oooooba <fuumin.23@gmail.com>
2026-08-11 23:30:30 +0900

    fix build.sh to specify build directory explicitly

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 debug_runtime=false
 debug_user=false
-build_directory=build
+build_directory=""
 
 while [ "$1" != "" ]; do
     case "$1" in
@@ -32,15 +32,19 @@ else
     cargo build >/dev/null 2>&1
 fi
 
-if [ -d $build_directory ]; then
-    rm -rf $build_directory
+if [ -n "$build_directory" ]; then
+    if [ -d "$build_directory" ]; then
+        rm -rf "$build_directory"
+    fi
+    mkdir -p "$build_directory"
+else
+    build_directory=`mktemp -d`
 fi
-mkdir -p $build_directory
 
 dir_name=$(cd `dirname $1` && pwd)
 base_name=`basename $1`
 src=$dir_name/$base_name
-cgen_args="-b ../$build_directory -i $src"
+cgen_args="-b `realpath $build_directory` -i $src"
 cd cgen
 go run main.go $cgen_args
 cd ..
