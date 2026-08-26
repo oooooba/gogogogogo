57fbea3d7ed3910620af2c49ac278afa51a1df91
oooooba <fuumin.23@gmail.com>
2026-08-26 22:07:39 +0900

    separate the contents of check_regression.sh

---
+++ check_go_compiler_tests.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+go_root=$(go env GOROOT)
+if [ ! -d "$go_root/test" ]; then
+    exit 0
+fi
+
+set +ex
+
+targets=(
+    "array"
+    "complit"
+    "convert"
+    "cplx0"
+    "cplx1"
+    "cplx2"
+    "cplx5"
+    "divmod"
+    "embed"
+    "for"
+    "interbasic"
+    "interfun"
+    "intervar"
+    "label"
+    "litfun"
+    "mfunc"
+    "ptrfun"
+    "ptrvar"
+    "range"
+    "rob1"
+    "robfor"
+    "robfunc"
+    "shift"
+    "simparray"
+    "simpbool"
+    "simpconv"
+    "simpfun"
+    "simpswitch"
+    "simpvar"
+    "slicearray"
+    "sliceslice"
+    "string"
+    "strvar"
+)
+
+function check_ken() {
+    local target="$1"
+    local path=$go_root/test/ken/$target.go
+
+    if ! bash ./check_equivalence.sh "$path"; then
+        echo "[$path] FAIL"
+        return 1
+    fi
+
+    return 0
+}
+
+export -f check_ken
+export go_root
+
+fail_count=0
+if ! printf '%s\n' "${targets[@]}" | xargs -P 3 -I {} bash -c 'check_ken "$@"' _ {}; then
+    fail_count=1
+fi
+
+if [ $fail_count -eq 0 ]; then
+    echo PASS
+else
+    echo FAIL
+fi
+
+exit $fail_count
